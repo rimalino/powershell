7c26cae01f600200bd6b59186bec7427f2bb886e
rimalino <32282976+rimalino@users.noreply.github.com>
2019-07-05 13:17:26 -0400

    Add files via upload

---
+++ get_vmname_ip.ps1
@@ -0,0 +1,10 @@
+﻿$vms = get-azvm
+$nics = get-aznetworkinterface | where VirtualMachine -NE $null #skip Nics with no VM
+
+foreach($nic in $nics)
+{
+    $vm = $vms | where-object -Property Id -EQ $nic.VirtualMachine.id
+    $prv =  $nic.IpConfigurations | select-object -ExpandProperty PrivateIpAddress
+    $alloc =  $nic.IpConfigurations | select-object -ExpandProperty PrivateIpAllocationMethod
+    Write-Output "$($vm.Name) , $prv"
+}
